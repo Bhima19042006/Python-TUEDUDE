1f8beed89ad6d7c84885a572940fe17ebb8a67e3
Bhimashankar Vilas Bake <bakebhimashankar1234@outlook.com>
2025-07-07 14:43:36 +0530

    Add files via upload

---
+++ Assignment_1.py
@@ -0,0 +1,19 @@
+#Task - 1
+
+a=int(input("Enter the first number : "))
+b=int(input("Enter the second number : "))
+c=a+b
+print("Addition : ",c)
+c=a-b
+print("Subtraction : ",c)
+c=a*b
+print("Multiplication : ",c)
+c=a/b
+print("Division : ",c)
+
+
+# Task - 2
+
+a=input("Enter your first name : ")
+b=input("Enter your last name : ")
+print("Hello,",a,b,"Welcome to the Python program.")
